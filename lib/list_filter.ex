dbf3c25bf3e8a0187781265bdf2f82fc1c2642ff
Alesandro Silva <silva_alesandro@outlook.com>
2021-03-10 23:20:22 -0400

    feat: create ListFilter.call/1 that returns how many odd nunbers there is from a generic list

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,9 @@
 defmodule ListFilter do
-  @moduledoc """
-  Documentation for `ListFilter`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ListFilter.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def call(list) do
+    list
+    |> Enum.filter(fn elem -> is_number(elem) end)
+    |> Enum.count(fn num -> is_odd(num) end)
   end
+
+  defp is_odd(num), do: rem(num, 2) != 0
 end
