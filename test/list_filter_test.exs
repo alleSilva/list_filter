5ed5296ef0137efe6aa40d4baa7bbaa0810c445c
Alesandro Silva <silva_alesandro@outlook.com>
2021-03-11 05:21:35 -0400

    test: ensure that ListFilter.call/1 returns how many odd nunbers there is from a generic list

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,15 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "returns how many odd nunbers there is from a generic list" do
+      list = [:a, "any", 0, 3, 6, 77, 88, 9, "banana", :carambola, 11, 25, 12]
+
+      response = ListFilter.call(list)
+
+      expected_response = 5
+
+      assert response == expected_response
+    end
   end
 end
